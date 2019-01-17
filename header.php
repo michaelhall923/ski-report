37445292ea4c821af36e7ee89e0040c0dc727e84
Akblizzard9 <shafran.flmason@gmail.com>
2019-01-16 17:29:24 -0800

    cleaning

--- header.php
+++
@@ -1,5 +0,0 @@
-<header>
-  <h1>This is the header</h1>
-  <img src = "drawing-2.svg" alt = "background" >
-  <img src="drawing.svg" alt="Logo">
-</header>
--- repos/ski-report/head.php
+++
@@ -1,4 +0,0 @@
-<meta charset="utf-8">
-<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
-
-<link rel="stylesheet" href="style.css">
